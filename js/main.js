52a8df74e2de6b2a6a4af6137e5609a5152d85c5
April Ding <apr.ding@gmail.com>
2019-05-26 18:29:54 -0700

    mirror example

--- js/main.js
+++ js/main.js
@@ -1,107 +1,144 @@
-if (WEBGL.isWebGLAvailable() === false) {
-    document.body.appendChild(WEBGL.getWebGLErrorMessage());
-}
-var container, stats, controls;
-var camera, scene, renderer, light, meshLoad, material;
-var clock = new THREE.Clock();
-var mixer;
+// scene size
+var WIDTH = window.innerWidth;
+var HEIGHT = window.innerHeight;
+// camera
+var VIEW_ANGLE = 45;
+var ASPECT = WIDTH / HEIGHT;
+var NEAR = 1;
+var FAR = 500;
+var camera, scene, renderer;
+var cameraControls;
+var sphereGroup, smallSphere;
 init();
 animate();
 
 function init() {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
-    camera.position.set(100,-300,300);
-    camera.lookAt(new THREE.Vector3(0,0,0));
-    controls = new THREE.OrbitControls(camera);
-    controls.target.set(0, 100, 0);
-    controls.update();
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xa0a0a0);
-    scene.fog = new THREE.Fog(0xa0a0a0, 200, 1000);
-    light = new THREE.HemisphereLight(0xffffff, 0x444444);
-    light.position.set(0, 200,0);
-    scene.add(light);
-    light = new THREE.DirectionalLight(0xffffff);
-    light.position.set(0, 200, 100);
-    light.castShadow = true;
-    light.shadow.camera.top = 180;
-    light.shadow.camera.bottom = -100;
-    light.shadow.camera.left = -120;
-    light.shadow.camera.right = 120;
-    scene.add(light);
-
-    material = new THREE.MeshNormalMaterial({metalness: 0.8, light: true, skinning: true, wireframe: true});
-    // scene.add( new THREE.CameraHelper( light.shadow.camera ) );
-
-    /******* Ground *********/
-    // var mesh = new THREE.Mesh(new THREE.PlaneBufferGeometry(2000, 2000), new THREE.MeshPhongMaterial({
-    //     color: 0x999999,
-    //     depthWrite: false
-    // }));
-    // mesh.rotation.x = -Math.PI / 2;
-    // mesh.receiveShadow = true;
-    // scene.add(mesh);
-
-    /******* Grid *********/
-    // var grid = new THREE.GridHelper(2000, 20, 0x000000, 0x000000);
-    // grid.material.opacity = 0.2;
-    // grid.material.transparent = true;
-    // scene.add(grid);
-
-    /******* Model *********/
-    var loader = new THREE.FBXLoader();
-    loader.load('img/hand4.fbx', function(object) {
-        object.scale.x += 10;
-        object.scale.y += 10;
-        object.scale.z += 10;
-        mixer = new THREE.AnimationMixer(object);
-        var action = mixer.clipAction(object.animations[0]);
-        action.play();
-        action.timeScale =  0.2;
-        object.traverse(function(child) {
-            if (child.isMesh) {
-                child.castShadow = true;
-                child.receiveShadow = true;
-                child.material = material;
-            }
-            // if ( child instanceof THREE.Mesh ) {
-            //     child.material = material;
-            // }
-        });
-        meshLoad = object;
-        scene.add(object);
-    });
-
-    /******* Renderer *********/
+    var container = document.getElementById('container');
+    // renderer
     renderer = new THREE.WebGLRenderer({
         antialias: true
     });
     renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.shadowMap.enabled = true;
+    renderer.setSize(WIDTH, HEIGHT);
     container.appendChild(renderer.domElement);
-    window.addEventListener('resize', onWindowResize, false);
-    // stats
-    stats = new Stats();
-    container.appendChild(stats.dom);
+    // scene
+    scene = new THREE.Scene();
+    // camera
+    camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+    camera.position.set(0, 75, 160);
+    cameraControls = new THREE.OrbitControls(camera, renderer.domElement);
+    cameraControls.target.set(0, 40, 0);
+    cameraControls.maxDistance = 400;
+    cameraControls.minDistance = 10;
+    cameraControls.update();
+    //
+    var planeGeo = new THREE.PlaneBufferGeometry(100.1, 100.1);
+    // reflectors/mirrors
+    var geometry = new THREE.CircleBufferGeometry(40, 64);
+    var groundMirror = new THREE.Reflector(geometry, {
+        clipBias: 0.003,
+        textureWidth: WIDTH * window.devicePixelRatio,
+        textureHeight: HEIGHT * window.devicePixelRatio,
+        color: 0x777777,
+        recursion: 1
+    });
+    groundMirror.position.y = 0.5;
+    groundMirror.rotateX(-Math.PI / 2);
+    scene.add(groundMirror);
+    var geometry = new THREE.PlaneBufferGeometry(100, 100);
+    var verticalMirror = new THREE.Reflector(geometry, {
+        clipBias: 0.003,
+        textureWidth: WIDTH * window.devicePixelRatio,
+        textureHeight: HEIGHT * window.devicePixelRatio,
+        color: 0x889999,
+        recursion: 1
+    });
+    verticalMirror.position.y = 50;
+    verticalMirror.position.z = -50;
+    scene.add(verticalMirror);
+    sphereGroup = new THREE.Object3D();
+    scene.add(sphereGroup);
+    var geometry = new THREE.CylinderBufferGeometry(0.1, 15 * Math.cos(Math.PI / 180 * 30), 0.1, 24, 1);
+    var material = new THREE.MeshPhongMaterial({
+        color: 0xffffff,
+        emissive: 0x444444
+    });
+    var sphereCap = new THREE.Mesh(geometry, material);
+    sphereCap.position.y = -15 * Math.sin(Math.PI / 180 * 30) - 0.05;
+    sphereCap.rotateX(-Math.PI);
+    var geometry = new THREE.SphereBufferGeometry(15, 24, 24, Math.PI / 2, Math.PI * 2, 0, Math.PI / 180 * 120);
+    var halfSphere = new THREE.Mesh(geometry, material);
+    halfSphere.add(sphereCap);
+    halfSphere.rotateX(-Math.PI / 180 * 135);
+    halfSphere.rotateZ(-Math.PI / 180 * 20);
+    halfSphere.position.y = 7.5 + 15 * Math.sin(Math.PI / 180 * 30);
+    sphereGroup.add(halfSphere);
+    var geometry = new THREE.IcosahedronBufferGeometry(5, 0);
+    var material = new THREE.MeshPhongMaterial({
+        color: 0xffffff,
+        emissive: 0x333333,
+        flatShading: true
+    });
+    smallSphere = new THREE.Mesh(geometry, material);
+    scene.add(smallSphere);
+    // walls
+    var planeTop = new THREE.Mesh(planeGeo, new THREE.MeshPhongMaterial({
+        color: 0xffffff
+    }));
+    planeTop.position.y = 100;
+    planeTop.rotateX(Math.PI / 2);
+    scene.add(planeTop);
+    var planeBottom = new THREE.Mesh(planeGeo, new THREE.MeshPhongMaterial({
+        color: 0xffffff
+    }));
+    planeBottom.rotateX(-Math.PI / 2);
+    scene.add(planeBottom);
+    var planeFront = new THREE.Mesh(planeGeo, new THREE.MeshPhongMaterial({
+        color: 0x7f7fff
+    }));
+    planeFront.position.z = 50;
+    planeFront.position.y = 50;
+    planeFront.rotateY(Math.PI);
+    scene.add(planeFront);
+    var planeRight = new THREE.Mesh(planeGeo, new THREE.MeshPhongMaterial({
+        color: 0x00ff00
+    }));
+    planeRight.position.x = 50;
+    planeRight.position.y = 50;
+    planeRight.rotateY(-Math.PI / 2);
+    scene.add(planeRight);
+    var planeLeft = new THREE.Mesh(planeGeo, new THREE.MeshPhongMaterial({
+        color: 0xff0000
+    }));
+    planeLeft.position.x = -50;
+    planeLeft.position.y = 50;
+    planeLeft.rotateY(Math.PI / 2);
+    scene.add(planeLeft);
+    // lights
+    var mainLight = new THREE.PointLight(0xcccccc, 1.5, 250);
+    mainLight.position.y = 60;
+    scene.add(mainLight);
+    var greenLight = new THREE.PointLight(0x00ff00, 0.25, 1000);
+    greenLight.position.set(550, 50, 0);
+    scene.add(greenLight);
+    var redLight = new THREE.PointLight(0xff0000, 0.25, 1000);
+    redLight.position.set(-550, 50, 0);
+    scene.add(redLight);
+    var blueLight = new THREE.PointLight(0x7f7fff, 0.25, 1000);
+    blueLight.position.set(0, 50, 550);
+    scene.add(blueLight);
 }
 
-function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-}
-//
 function animate() {
     requestAnimationFrame(animate);
-    // meshLoad.rotation.x += 0.01;
-    // meshLoad.rotation.y += 0.01;
-
-
-    var delta = clock.getDelta();
-    if (mixer) mixer.update(delta);
+    var timer = Date.now() * 0.01;
+    sphereGroup.rotation.y -= 0.002;
+    smallSphere.position.set(
+        Math.cos(timer * 0.1) * 30,
+        Math.abs(Math.cos(timer * 0.2)) * 20 + 5,
+        Math.sin(timer * 0.1) * 30
+    );
+    smallSphere.rotation.y = (Math.PI / 2) - timer * 0.1;
+    smallSphere.rotation.z = timer * 0.8;
     renderer.render(scene, camera);
-    stats.update();
 }
