979ee00134adf11741e89b383fee6aef31f96ffe
April Ding <apr.ding@gmail.com>
2019-05-27 20:33:01 -0700

    cleanup main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+// ======================================================================
+// global variables
+// ======================================================================
 // scene size
 var WIDTH = window.innerWidth;
 var HEIGHT = window.innerHeight;
@@ -8,18 +11,31 @@ var NEAR = 1;
 var FAR = 500;
 var camera, scene, renderer;
 var cameraControls;
-var sphereGroup, smallSphere;
+//lights
 
-var meshLoad;
-
+//for animations
 var clock = new THREE.Clock();
-			var mixer;
+var mixer;
 
+// ======================================================================
+// call functions
+// ======================================================================
 init();
 animate();
 
+// ======================================================================
+// define functions
+// ======================================================================
 function init() {
     var container = document.getElementById('container');
+
+    createScene();
+    createLights();
+    createCube();
+
+}
+
+function createScene() {
     // renderer
     renderer = new THREE.WebGLRenderer({
         alpha: true,
@@ -38,94 +54,83 @@ function init() {
     cameraControls.maxDistance = 800;
     cameraControls.minDistance = 10;
     cameraControls.update();
-    //
-    var planeGeo = new THREE.PlaneBufferGeometry(100.1, 100.1);
-    // reflectors/mirrors
-    var geometry = new THREE.CircleBufferGeometry(400, 640);
-    var groundMirror = new THREE.Reflector(geometry, {
-        clipBias: 0.003,
-        textureWidth: WIDTH * window.devicePixelRatio,
-        textureHeight: HEIGHT * window.devicePixelRatio,
-        color: 0x777777,
-        recursion: 1
-    });
-    groundMirror.position.y = 10;
-    groundMirror.rotateX(-Math.PI / 2);
-    //scene.add(groundMirror);
+}
 
-    // var geometry = new THREE.PlaneBufferGeometry(100, 100);
-    // var verticalMirror = new THREE.Reflector(geometry, {
-    //     clipBias: 0.003,
-    //     textureWidth: WIDTH * window.devicePixelRatio,
-    //     textureHeight: HEIGHT * window.devicePixelRatio,
-    //     color: 0x889999,
-    //     recursion: 1
-    // });
-    // verticalMirror.position.y = 50;
-    // verticalMirror.position.z = -100;
-    // scene.add(verticalMirror);
-    // //----
-    // var geometry2 = new THREE.PlaneBufferGeometry(100, 100);
-    // var verticalMirror2 = new THREE.Reflector(geometry2, {
-    //     clipBias: 0.003,
-    //     textureWidth: WIDTH * window.devicePixelRatio,
-    //     textureHeight: HEIGHT * window.devicePixelRatio,
-    //     color: 0x889999,
-    //     recursion: 1
-    // });
-    // verticalMirror2.position.y = 50;
-    // verticalMirror2.position.x = -100;
-    // verticalMirror2.rotateY(Math.PI / 2);
-    //
-    // scene.add(verticalMirror2);
-    // //----
-    // var geometry3 = new THREE.PlaneBufferGeometry(100, 100);
-    // var verticalMirror3 = new THREE.Reflector(geometry3, {
-    //     clipBias: 0.003,
-    //     textureWidth: WIDTH * window.devicePixelRatio,
-    //     textureHeight: HEIGHT * window.devicePixelRatio,
-    //     color: 0x889999,
-    //     recursion: 1
-    // });
-    // verticalMirror3.position.y = 50;
-    // verticalMirror3.position.x = 100;
-    // verticalMirror3.position.z = 0;
-    // verticalMirror3.rotateY(-Math.PI / 2);
-    //
-    // scene.add(verticalMirror3);
+function createLights(){
+
+    // var mainLight = new THREE.PointLight(0xcccccc, 1.5, 250);
+    // mainLight.position.y = 60;
+    // scene.add(mainLight);
+    // var greenLight = new THREE.PointLight(0x00ff00, 0.25, 1000);
+    // greenLight.position.set(550, 50, 0);
+    // scene.add(greenLight);
+    // var redLight = new THREE.PointLight(0xff0000, 0.25, 1000);
+    // redLight.position.set(-550, 50, 0);
+    // scene.add(redLight);
+    // var blueLight = new THREE.PointLight(0x7f7fff, 0.25, 1000);
+    // blueLight.position.set(0, 50, 550);
+    // scene.add(blueLight);
 
 
-    /******* Model *********/
+    var hemisphereLight, shadowLight;
+    // A hemisphere light is a gradient colored light;
+    // the first parameter is the sky color, the second parameter is the ground color,
+    // the third parameter is the intensity of the light
+    hemisphereLight = new THREE.HemisphereLight(0xaaaaaa, 0x000000, .5)
+
+    // A directional light shines from a specific direction.
+    // It acts like the sun, that means that all the rays produced are parallel.
+    shadowLight = new THREE.DirectionalLight(0xffffff, .8);
+
+    // Set the direction of the light
+    shadowLight.position.set(150, 350, 350);
+
+    // Allow shadow casting
+    shadowLight.castShadow = true;
+
+    // define the visible area of the projected shadow
+    shadowLight.shadow.camera.left = -400;
+    shadowLight.shadow.camera.right = 400;
+    shadowLight.shadow.camera.top = 400;
+    shadowLight.shadow.camera.bottom = -400;
+    shadowLight.shadow.camera.near = 1;
+    shadowLight.shadow.camera.far = 1000;
+
+    // define the resolution of the shadow; the higher the better,
+    // but also the more expensive and less performant
+    shadowLight.shadow.mapSize.width = 2048;
+    shadowLight.shadow.mapSize.height = 2048;
+
+    // to activate the lights, just add them to the scene
+    scene.add(hemisphereLight);
+    scene.add(shadowLight);
+}
+
+function createCube(){
+    /********************* Room Scene ***********************/
     var loader = new THREE.FBXLoader();
     var roomMat = new THREE.MeshToonMaterial({
         color: 0xb3c4aa,
 
     });
 
-
     loader.load('img/room1.fbx', function(object) {
         object.scale.x += 5;
         object.scale.y += 5;
         object.scale.z += 5;
-        //mixer = new THREE.AnimationMixer(object);
-        //var action = mixer.clipAction(object.animations[0]);
-        //action.play();
-        //action.timeScale =  0.2;
         object.traverse(function(child) {
             if (child.isMesh) {
                 child.castShadow = true;
                 child.receiveShadow = true;
                 child.material = roomMat;
             }
-            // if ( child instanceof THREE.Mesh ) {
-            //     child.material = material;
-            // }
         });
 
         meshLoad = object;
         scene.add(object);
     });
 
+    /********************* Cube Frame ***********************/
     var frame = new THREE.FBXLoader();
     var flatMat = new THREE.MeshToonMaterial({
         color: 0xf4f8ff,
@@ -138,24 +143,18 @@ function init() {
         object.scale.x += 5;
         object.scale.y += 5;
         object.scale.z += 5;
-        //mixer = new THREE.AnimationMixer(object);
-        //var action = mixer.clipAction(object.animations[0]);
-        //action.play();
-        //action.timeScale =  0.2;
         object.traverse(function(child) {
             if (child.isMesh) {
                 child.castShadow = true;
                 child.receiveShadow = true;
                 child.material = flatMat;
             }
-            // if ( child instanceof THREE.Mesh ) {
-            //     child.material = material;
-            // }
         });
 
         scene.add(object);
     });
 
+    /********************* Person ***********************/
     var person = new THREE.FBXLoader();
     var personMat = new THREE.MeshToonMaterial({
         color: 0xf9b9a7,
@@ -170,7 +169,7 @@ function init() {
         mixer = new THREE.AnimationMixer(object);
         var action = mixer.clipAction(object.animations[0]);
         action.play();
-        //action.timeScale =  0.2;
+        //action.timeScale =  0.2; //controls the speed of the animation
         object.traverse(function(child) {
             if (child.isMesh) {
                 child.castShadow = true;
@@ -182,126 +181,6 @@ function init() {
         scene.add(object);
     });
 
-
-
-
-
-
-    // sphereGroup = new THREE.Object3D();
-    // scene.add(sphereGroup);
-    // var geometry = new THREE.CylinderBufferGeometry(0.1, 15 * Math.cos(Math.PI / 180 * 30), 0.1, 24, 1);
-    // var material = new THREE.MeshToonMaterial({
-    //     color: 0xa9c5f9,
-    //     //emissive: 0xa9c5f9,
-    //     flatShading: true
-    // });
-    // var sphereCap = new THREE.Mesh(geometry, material);
-    // sphereCap.position.y = -15 * Math.sin(Math.PI / 180 * 30) - 0.05;
-    // sphereCap.rotateX(-Math.PI);
-    // var geometry = new THREE.SphereBufferGeometry(15, 24, 24, Math.PI / 2, Math.PI * 2, 0, Math.PI / 180 * 120);
-    // var halfSphere = new THREE.Mesh(geometry, material);
-    // halfSphere.add(sphereCap);
-    // halfSphere.rotateX(-Math.PI / 180 * 135);
-    // halfSphere.rotateZ(-Math.PI / 180 * 20);
-    // halfSphere.position.y = 7.5 + 15 * Math.sin(Math.PI / 180 * 30);
-    // sphereGroup.add(halfSphere);
-    // var geometry = new THREE.IcosahedronBufferGeometry(5, 0);
-    // var material = new THREE.MeshToonMaterial({
-    //     color: 0xfffdc6,
-    //     //emissive: 0xfffdc6,
-    //     flatShading: true
-    // });
-    // smallSphere = new THREE.Mesh(geometry, material);
-    // scene.add(smallSphere);
-
-
-    // walls
-    // var planeTop = new THREE.Mesh(planeGeo, new THREE.MeshPhongMaterial({
-    //     color: 0xffffff
-    // }));
-    // planeTop.position.y = 100;
-    // planeTop.rotateX(Math.PI / 2);
-    // scene.add(planeTop);
-
-    // var planeBottom = new THREE.Mesh(planeGeo, new THREE.MeshPhongMaterial({
-    //     color: 0xffffff
-    // }));
-    // planeBottom.rotateX(-Math.PI / 2);
-    // scene.add(planeBottom);
-    // var planeFront = new THREE.Mesh(planeGeo, new THREE.MeshPhongMaterial({
-    //     color: 0x7f7fff
-    // }));
-    // planeFront.position.z = 50;
-    // planeFront.position.y = 50;
-    // planeFront.rotateY(Math.PI);
-    // scene.add(planeFront);
-    // var planeRight = new THREE.Mesh(planeGeo, new THREE.MeshPhongMaterial({
-    //     color: 0x00ff00
-    // }));
-    // planeRight.position.x = 50;
-    // planeRight.position.y = 50;
-    // planeRight.rotateY(-Math.PI / 2);
-    // scene.add(planeRight);
-    // var planeLeft = new THREE.Mesh(planeGeo, new THREE.MeshPhongMaterial({
-    //     color: 0xff0000
-    // }));
-    // planeLeft.position.x = -50;
-    // planeLeft.position.y = 50;
-    // planeLeft.rotateY(Math.PI / 2);
-    // scene.add(planeLeft);
-
-
-    // lights
-
-    var mainLight = new THREE.PointLight(0xcccccc, 1.5, 250);
-    mainLight.position.y = 60;
-    scene.add(mainLight);
-    var greenLight = new THREE.PointLight(0x00ff00, 0.25, 1000);
-    greenLight.position.set(550, 50, 0);
-    scene.add(greenLight);
-    var redLight = new THREE.PointLight(0xff0000, 0.25, 1000);
-    redLight.position.set(-550, 50, 0);
-    scene.add(redLight);
-    var blueLight = new THREE.PointLight(0x7f7fff, 0.25, 1000);
-    blueLight.position.set(0, 50, 550);
-    scene.add(blueLight);
-
-
-    // var hemisphereLight, shadowLight;
-    //
-    // function createLights() {
-    //     // A hemisphere light is a gradient colored light;
-    //     // the first parameter is the sky color, the second parameter is the ground color,
-    //     // the third parameter is the intensity of the light
-    //     hemisphereLight = new THREE.HemisphereLight(0xaaaaaa, 0x000000, .9)
-    //
-    //     // A directional light shines from a specific direction.
-    //     // It acts like the sun, that means that all the rays produced are parallel.
-    //     shadowLight = new THREE.DirectionalLight(0xffffff, .9);
-    //
-    //     // Set the direction of the light
-    //     shadowLight.position.set(150, 350, 350);
-    //
-    //     // Allow shadow casting
-    //     shadowLight.castShadow = true;
-    //
-    //     // define the visible area of the projected shadow
-    //     shadowLight.shadow.camera.left = -400;
-    //     shadowLight.shadow.camera.right = 400;
-    //     shadowLight.shadow.camera.top = 400;
-    //     shadowLight.shadow.camera.bottom = -400;
-    //     shadowLight.shadow.camera.near = 1;
-    //     shadowLight.shadow.camera.far = 1000;
-    //
-    //     // define the resolution of the shadow; the higher the better,
-    //     // but also the more expensive and less performant
-    //     shadowLight.shadow.mapSize.width = 2048;
-    //     shadowLight.shadow.mapSize.height = 2048;
-    //
-    //     // to activate the lights, just add them to the scene
-    //     scene.add(hemisphereLight);
-    //     scene.add(shadowLight);
-    // }
 }
 
 function animate() {
@@ -317,6 +196,8 @@ function animate() {
     //meshLoad.rotation.z = timer * 0.8;
 
     var delta = clock.getDelta();
-	if ( mixer ) mixer.update( delta );
+    if (mixer) mixer.update(delta);
+
+
     renderer.render(scene, camera);
 }
